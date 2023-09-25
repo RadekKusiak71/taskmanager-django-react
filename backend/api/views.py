8049aab1f0b11b0e589706f03477981e5ad62eb0
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-09-26 01:45:22 +0200

    added apiOverview for test

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+
+
+@api_view(['GET'])
+def getOverview(request):
+    data = {
+        'api state': True
+    }
+    return Response(data)
