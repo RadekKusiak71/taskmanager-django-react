54a8b3f139132557b2da14930c4ca80fb8a6e413
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-09-26 21:49:25 +0200

    input/wrapper adjust

--- frontend/src/components/UI/Input.js
+++ frontend/src/components/UI/Input.js
@@ -4,7 +4,7 @@ import classes from './Input.module.css'
 const Input = (props) => {
   return (
     <input
-      className={classes.input}
+        className={classes.input}
         onChange={props.onChange}
         type={props.type}
         id={props.id}
