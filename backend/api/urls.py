d9dc619cea594fc1cd00a32b25014bdb3f8b6232
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-09-26 02:25:26 +0200

    changed url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getOverview)
+    path('',views.getOverview),
+    path('tasks/<int:user_id>/',views.getTasks),
 ]
