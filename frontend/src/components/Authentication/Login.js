533ad0ec52281f271a654cab307f5f9347bff22b
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-09-26 16:12:04 +0200

    Login/Register form setup for api call

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,9 +1,66 @@
-import React from 'react'
-import classes from './Login.module.css'
+import React, { useState } from 'react'
+import FormBox from '../UI/FormBox'
+import Card from '../UI/Card'
+import Input from '../UI/Input'
+import Button from '../UI/Button'
+
+
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    const [formErrors, setFormErrors] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const validateForm = () => {
+        let errors = {}
+        let isValid = true;
+
+        if (formData.username.trim() === '') {
+            errors.username = 'Username is required';
+            isValid = false;
+        }
+        if (formData.password.length < 6) {
+            errors.password = 'Password must be at least 6 characters';
+            isValid = false;
+        }
+        setFormErrors(errors);
+        return isValid;
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (validateForm()) {
+            console.log('Registration successful');
+        }
+    }
+
+
     return (
-        <div>Login</div>
+        <Card>
+            <FormBox form-text={'You dont have an account yet? Sign up for free...'} path={'/register'} onSubmit={submitHandler}>
+                <Input onChange={handleChange} type='text' id='username' name='username' placeholder='Username' value={formData.username} />
+                {formErrors.username && (
+                    <div className='error-message'>{formErrors.username}</div>
+                )}
+                <Input onChange={handleChange} type='password' id='password' name='password' placeholder='Password' value={formData.password} />
+                {formErrors.password && (
+                    <div className='error-message'>{formErrors.password}</div>
+                )}
+                <Button type={'submit'}>Login</Button>
+            </FormBox>
+        </Card>
     )
 }
 
--- frontend/src/components/Authentication/Register.js
+++ frontend/src/components/Authentication/Register.js
@@ -1,10 +1,148 @@
-import React from 'react'
-import classes from './Register.module.css'
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import FormBox from '../UI/FormBox';
+import Input from '../UI/Input';
+import Button from '../UI/Button';
 
 const Register = () => {
-    return (
-        <div>Register</div>
-    )
-}
+    const [formData, setFormData] = useState({
+        username: '',
+        firstname: '',
+        lastname: '',
+        password: '',
+        passwordConfirm: '',
+    });
 
-export default Register
+    const [formErrors, setFormErrors] = useState({
+        username: '',
+        firstname: '',
+        lastname: '',
+        password: '',
+        passwordConfirm: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const validateForm = () => {
+        let errors = {};
+        let isValid = true;
+
+        // Validation rules (you can customize these)
+        if (formData.username.trim() === '') {
+            errors.username = 'Username is required';
+            isValid = false;
+        }
+
+        if (formData.firstname.trim() === '') {
+            errors.firstname = 'First name is required';
+            isValid = false;
+        }
+
+        if (formData.lastname.trim() === '') {
+            errors.lastname = 'Last name is required';
+            isValid = false;
+        }
+
+        if (formData.password.length < 6) {
+            errors.password = 'Password must be at least 6 characters';
+            isValid = false;
+        }
+
+        if (formData.password !== formData.passwordConfirm) {
+            errors.passwordConfirm = 'Passwords do not match';
+            isValid = false;
+        }
+
+        setFormErrors(errors);
+        return isValid;
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (validateForm()) {
+            console.log('Registration successful');
+            //api call
+        }
+    };
+
+    return (
+        <Card>
+            <FormBox
+                form-text={'You have an account already? Login'}
+                path={'/login'}
+                onSubmit={submitHandler}
+            >
+                <Input
+                    onChange={handleChange}
+                    type='text'
+                    id='username'
+                    name='username'
+                    placeholder='Username'
+                    value={formData.username}
+                />
+                {formErrors.username && (
+                    <div className='error-message'>{formErrors.username}</div>
+                )}
+
+                <Input
+                    onChange={handleChange}
+                    type='text'
+                    id='first_name'
+                    name='firstname'
+                    placeholder='First name'
+                    value={formData.firstname}
+                />
+                {formErrors.firstname && (
+                    <div className='error-message'>{formErrors.firstname}</div>
+                )}
+
+                <Input
+                    onChange={handleChange}
+                    type='text'
+                    id='last_name'
+                    name='lastname'
+                    placeholder='Last name'
+                    value={formData.lastname}
+                />
+                {formErrors.lastname && (
+                    <div className='error-message'>{formErrors.lastname}</div>
+                )}
+
+                <Input
+                    onChange={handleChange}
+                    type='password'
+                    id='password'
+                    name='password'
+                    placeholder='Password'
+                    value={formData.password}
+                />
+                {formErrors.password && (
+                    <div className='error-message'>{formErrors.password}</div>
+                )}
+
+                <Input
+                    onChange={handleChange}
+                    type='password'
+                    id='password_confirm'
+                    name='passwordConfirm'
+                    placeholder='Re-type password'
+                    value={formData.passwordConfirm}
+                />
+                {formErrors.passwordConfirm && (
+                    <div className='error-message'>{formErrors.passwordConfirm}</div>
+                )}
+
+                <Button type={'submit'}>Register</Button>
+            </FormBox>
+        </Card>
+    );
+};
+
+export default Register;
