c95cee6dc9088f00d82a50ee940987d9b34b36c0
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-09-26 14:59:04 +0200

    react-router-dom setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Header from './components/Header/Header'
+import Login from './components/Authentication/Login'
+import Register from './components/Authentication/Register'
+import Tasks from './components/Tasks/Task'
+import Task from "./components/Tasks/Task";
+import TasksCompleted from "./components/Tasks/TasksCompleted";
+import TaskUncompleted from "./components/Tasks/TasksUncompleted";
 function App() {
   return (
-    <div>
-      <p>TaskManager</p>
-    </div>
+    <BrowserRouter>
+      <div className='main-container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={Tasks}/>
+          <Route path='/task' element={Task}/>
+          <Route path='/tasks/completed' element={TasksCompleted}/>
+          <Route path='/tasks/uncompleted' element={TaskUncompleted}/>
+          <Route path='login' element={Login}/>
+          <Route path='register' element={Register}/>
+          <Route/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
